3b1804c70c225a4cd63ab6b7dd2f121cb9fe4c83
SimonSungm <simonsungm@gmail.com>
2021-05-29 00:08:55 +0800

    init

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vimrc.d/bundle/Vundle.vim
+
+ln -s .vimrc.d/vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,6 @@
+" Add runtime
+
+set rtp+=~/.vimrc.d/runtime
+
+runtime __plugin_manager.vim
+runtime __key_binding.vim
